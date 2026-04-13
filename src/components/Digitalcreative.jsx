259431dc8d476b12f904925fcb129af903398d69
sudhagar712 <sudhagarmsr712@gmail.com>
2026-04-13 19:52:54 +0530

    xssx

--- src/components/Digitalcreative.jsx
+++ src/components/Digitalcreative.jsx
@@ -105,6 +105,7 @@ const DigitalCreativeGrid = () => {
               >
                 {/* Background Image with optional Metro Tinting */}
                 <img
+                  loading="lazy"
                   src={item.img}
                   alt={item.title}
                   // Raw image is revealed upon hover naturally by dropping the blend mode!
